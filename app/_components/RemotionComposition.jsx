8b645394d2daf108a6f4bad7ef9ed852ac36ac19
Sumit-Kushwah-786 <sumitkushwah33221@gmail.com>
2025-05-16 22:48:42 +0530

    Initial commit for AI Short Video Creator

---
+++ app/_components/RemotionComposition.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RemotionComposition() {
+  return <div>RemotionComposition</div>;
+}
+
+export default RemotionComposition;
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "example.com",
-      "firebasestorage.googleapis.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        // optional: add pathname if needed
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/v0/b/projects-2025-71366.firebasestorage.app/o/**",
+      },
+      {
+        protocol: "https",
+        hostname: "projects-2025-71366.firebaseapp.com",
+      },
     ],
   },
 };
